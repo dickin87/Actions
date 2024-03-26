b85ee41b8ded406905a3ccc985a8ae323532b072
Dickinson, Matthew William <dickin87@students.rowan.edu>
2024-03-26 15:56:20 -0400

    Attempt to run Python app with Docker container

--- example.py
+++ example.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 
-print("Hello world!")
+from flask import Flask, render_template
+import os
+import unittest
+
+appp = Flask(__name__)
+
+_output = "Test 1"
+
+class TestMethods(unittest.TestCase):
+	def test_anything(self):
+		self.assertEqual('1', '1')
+		self.assertEqual('Test' in _output)
+
+@app.route('/')
+def index():
+	return _output
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
